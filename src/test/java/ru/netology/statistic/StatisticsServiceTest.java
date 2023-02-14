6c2d69284e8b7c5c3ae37c4acf668e6de03165b1
KleoVor <t9671253993@gmail.com>
2023-02-14 09:50:44 +0300

    added test findMaxIfFalse

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfFalse(){
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 18};
+        long expected = 18;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
+
+
 }
